11fe928834f6f1053d74746dec646ff7c0ffb94c
iwakoshi <iwakoshi@jms-web.co.jp>
2021-12-15 12:56:27 +0900

    プロジェクト ファイルを追加します。

---
+++ HENKAN_Excel/FrmMain.vb
@@ -0,0 +1,19 @@
+﻿Public Class FrmMain
+
+    Sub New()
+
+        ' この呼び出しはデザイナーで必要です。
+        InitializeComponent()
+
+        ' InitializeComponent() 呼び出しの後で初期化を追加します。        
+
+        ''Form初期設定
+        '最大サイズと最小サイズを現在のサイズに設定する
+        Me.MaximumSize = Me.Size
+        Me.MinimumSize = Me.Size
+        'フォームが最大化されないようにする
+        Me.MaximizeBox = False
+
+    End Sub
+
+End Class
